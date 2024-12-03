75721b1d674d5aec2b9fac9f5f47a4c41de5d3d1
Samir Haddadi <sam20232022@gmail.com>
2024-12-03 17:19:04 +0000

    comments

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,11 +9,10 @@ route = express.Router();
 
 route.use(session({
     secret: 'keyboard cat',
-    resave: false ,
+    resave: false,
     saveUninitialized: true
 })) 
-
-
+ 
 // seeds the database with prototype data
 route.get('/seed',async(req,res)=>{
     userModel.collection.drop() // delete the collection document
